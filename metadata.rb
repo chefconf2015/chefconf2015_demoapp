d02a735b319cb04c38735fa7de43ee83af078fe1
shashirsa <shashishop@gmail.com>
2015-03-28 15:06:24 -0700

    Added dependencies

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,11 @@ description      'Installs/Configures chefconf2015_demoapp'
 long_description 'Installs/Configures chefconf2015_demoapp'
 version          '0.1.6'
 
+depends          'intu_java',        '= 0.1.9'
+depends          'intu_apache',      '= 2.0.0'
+depends          'intu_tomcat',      '= 0.1.3'
+depends          'intu_chef-splunk',  '= 2.0.1'
+depends          'intu_wily',        '= 1.0.3’
+
 source_url 'https://github.com/shashirsa/chefconf2015_demoapp'
 issues_url 'https://github.com/shashirsa/chefconf2015_demoapp/issues'
