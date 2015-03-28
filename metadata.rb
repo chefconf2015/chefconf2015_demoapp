8b3f2c8ed0d44a42f1fd9879b75466ac1a17d139
shashirsa <shashishop@gmail.com>
2015-03-28 16:09:41 -0700

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,13 +4,13 @@ maintainer_email 'intuchef@intuit.com'
 license          'All rights reserved'
 description      'Installs/Configures chefconf2015_demoapp'
 long_description 'Installs/Configures chefconf2015_demoapp'
-version          '0.1.8'
+version          '0.1.9'
 
 depends          'intu_java',        '= 0.1.9'
 depends          'intu_apache',      '= 2.0.0'
 depends          'intu_tomcat',      '= 0.1.3'
-depends          'intu_chef-splunk',  '= 2.0.1'
-depends          'intu_wily',        '= 1.0.3’
+depends          'intu_chef-splunk', '= 2.0.1'
+depends          'intu_wily',        '= 1.0.3'
 
 source_url "https://github.com/shashirsa/chefconf2015_demoapp"
 issues_url "https://github.com/shashirsa/chefconf2015_demoapp/issues"
