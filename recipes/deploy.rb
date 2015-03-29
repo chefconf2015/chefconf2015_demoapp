3d365b6eb419f59ce8432dfecdbb77ff1ec28614
shashirsa <shashishop@gmail.com>
2015-03-29 15:11:05 -0700

    Create deploy.rb

---
+++ recipes/deploy.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: chefconf2015_demoapp
+# Recipe:: deploy
+#
+# Copyright (C) 2015 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+ruby_block 'Deploying Demo App' do
+	block do
+		puts " "
+		puts "chefconf2015_demoapp: ...."
+		puts "chefconf2015_demoapp: Installed demo application"
+		puts "chefconf2015_demoapp: ...."
+	end
+end
