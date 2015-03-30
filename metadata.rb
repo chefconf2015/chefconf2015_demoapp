2ae14b262538ebc04972ab030e57143e13d66127
shashirsa <shashishop@gmail.com>
2015-03-29 18:03:47 -0700

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -10,7 +10,7 @@ depends          'intu_java',        '= 0.1.9'
 depends          'intu_apache',      '= 2.0.0'
 depends          'intu_tomcat',      '= 0.1.3'
 depends          'intu_chef-splunk', '= 2.0.1'
-depends          'intu_wily',        '= 1.0.3'
+depends          'intu_newrelic',    '= 0.1.4'
 
 source_url "https://github.com/chefconf2015/chefconf2015_demoapp"
 issues_url "https://github.com/chefconf2015/chefconf2015_demoapp/issues"
