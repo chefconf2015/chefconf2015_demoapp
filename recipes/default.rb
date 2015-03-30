29528f2688fa49676eceff8945f5e0b287534466
shashirsa <shashishop@gmail.com>
2015-03-29 18:04:32 -0700

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,7 +10,8 @@
 include_recipe 'intu_apache'
 include_recipe 'intu_java'
 include_recipe 'intu_tomcat'
-include_recipe 'intu_wily'
+include_recipe 'intu_newrelic'
 include_recipe 'intu_chef-splunk'
 
 include_recipe 'chefconf2015_demoapp::deploy'
+ 
